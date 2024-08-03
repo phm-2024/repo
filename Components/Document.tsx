0ea94b0bc82136ae5efb2506329a1b3bd014b038
Haruka Ogino <harukariq@gmail.com>
2024-08-03 14:08:10 +1200

    add form state

--- Components/Document.tsx
+++ Components/Document.tsx
@@ -1,26 +1,33 @@
 import { useState } from 'react'
+import DisappearingText from './DisappearingText'
 
 export default function Document() {
   const [input, setInput] = useState('')
+  const [form, setForm] = useState('')
+
+  const [disappear, setDisappear] = useState(false)
   console.log(input)
 
   function submission(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    setInput(input)
+    setInput(form)
+    setForm('')
+    setDisappear((prev) => !prev)
   }
 
   return (
     <>
       <form onSubmit={submission}>
         <input
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
+          onChange={(e) => setForm(e.target.value)}
+          value={form}
           className="border-solid border-2 border-indigo-600"
         />
         <button type="submit">Submit</button>
       </form>
       <br />
-      <p>{input}</p>
+      {disappear && <DisappearingText text={input} />}
+      {/* <p>{input}</p> */}
     </>
   )
 }
