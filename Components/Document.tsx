0313ed13d5cc35580a451b1d0c63df5a6a7a4bf2
Haruka Ogino <harukariq@gmail.com>
2024-08-03 13:56:19 +1200

    update Document component

--- Components/Document.tsx
+++ Components/Document.tsx
@@ -4,15 +4,20 @@ export default function Document() {
   const [input, setInput] = useState('')
   console.log(input)
 
+  function submission(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setInput(input)
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={submission}>
         <input
           onChange={(e) => setInput(e.target.value)}
           value={input}
           className="border-solid border-2 border-indigo-600"
         />
-        {/* <button type="submit">Submit</button> */}
+        <button type="submit">Submit</button>
       </form>
       <br />
       <p>{input}</p>
