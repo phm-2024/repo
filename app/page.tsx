1421f673305e81b7086e23fe595a5bd272bf4873
Moa Ha <moahaa03@gmail.com>
2024-08-03 16:01:16 +1200

    conditionally render component

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,34 @@
 import Password from '../components/Password'
 import Document from '../components/Document'
 import { useState } from 'react'
+import LoadData from '@/components/LoadData'
 
 export default function Home() {
   const [passkey, setPasskey] = useState('')
+  const [activeComponent, setActiveComponent] = useState('')
+
+  const renderComponent = () => {
+    if (activeComponent === 'New') {
+      return (
+        <>
+          {passkey === '' ? <Password setPasskey={setPasskey} /> : <Document />}
+        </>
+      )
+    } else if (activeComponent === 'LoadData') {
+      return <LoadData />
+    }
+    return null
+  }
 
   return (
-    <>
-      {passkey === '' ? <Password setPasskey={setPasskey} /> : <Document />}
-      {/* <>{passkey}</> */}
-    </>
+    <div>
+      <button className="p-16" onClick={() => setActiveComponent('New')}>
+        NEW
+      </button>
+      <button className="p-16" onClick={() => setActiveComponent('LoadData')}>
+        LOAD
+      </button>
+      {renderComponent()}
+    </div>
   )
 }
