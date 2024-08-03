f5e8e928357c739c166f3346482bb9a354b08340
Prashniel Kumar <prashniel@gmail.com>
2024-08-03 15:23:49 +1200

    clarify input forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 'use client'
-import Password from '@/components/Password'
-import Document from '@/components/Document'
+import Password from '../components/Password'
+import Document from '../components/Document'
 import { useState } from 'react'
-import Title from '@/components/Title'
 
 export default function Home() {
   const [passkey, setPasskey] = useState('')
 
   return (
     <>
-      <Title />
       {passkey === '' ? <Password setPasskey={setPasskey} /> : <Document />}
-      <>{passkey}</>
+      {/* <>{passkey}</> */}
     </>
   )
 }
