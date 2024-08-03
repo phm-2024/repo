c3163ecf00b562139e49394107dda22c556f7a7b
Prashniel Kumar <prashniel@gmail.com>
2024-08-03 14:16:33 +1200

    stop hiding attempt

--- Components/Document.tsx
+++ Components/Document.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
+
 export default function Document() {
+  let char = 0
+
+  useEffect(() => {
+    setTimeout(() => {}, 1000)
+  }, [charCount])
+
+  const [charCount, setCharCount] = useState(-1)
+  const [onText, setOnText] = useState('')
+
+  function expireText(e: any) {
+    setOnText(e.target.value)
+    setCharCount(charCount + 1)
+  }
+
   return (
     <>
-      <p>Document</p>
+      <p style={{ color: 'red' }}>{offText}</p>
+      <input value={onText} onChange={expireText} />
     </>
   )
 }
