ae52fcf0df1a3277fe990237e570caaa95614d1f
Haruka Ogino <harukariq@gmail.com>
2024-08-04 15:07:29 +1200

    create BE PATCH route

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -16,3 +16,22 @@ export const GET = async (
     return new Response('Failed to fetch projects', { status: 500 })
   }
 }
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params
+    const { file_name, notes } = await req.json()
+
+    const query = 'UPDATE notes SET file_name = ?, notes = ? WHERE user_id = ?'
+    const args = [file_name, notes, id]
+
+    const result = await turso.execute({ sql: query, args })
+
+    return new Response(JSON.stringify(result), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to fetch projects', { status: 500 })
+  }
+}
